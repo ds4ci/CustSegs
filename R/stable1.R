e64dd16dc45f030778db470fb065fe8ee500e863
Jim Porzak <jim@ds4ci.org>
2015-06-14 23:08:22 -0700

    added density plot of solutions

--- R/stable1.R
+++ R/stable1.R
@@ -30,15 +30,32 @@ for (itry in 1:num_trys) {
 
 # set up plot of size of rank 2 x rank 1
 cli_sizes <- cli_trys %>%
-  select(k, seed, clust_rank, size) %>%
+  dplyr::select(k, seed, clust_num, clust_rank, size) %>%
   filter(clust_rank <= 2) %>%
   mutate(clust_label = paste0("Size_", clust_rank)) %>%
-  select(-clust_rank) %>%
-  spread(key = clust_label, value = size)
+  dplyr::select(-clust_rank) %>%
+  spread(key = clust_label, value = size) %>%
+  group_by(k, seed) %>%
+  summarize(c1 = first(clust_num),
+            c2 = last(clust_num),
+            Size_1 = min(Size_1, na.rm = TRUE),
+            Size_2 = min(Size_2, na.rm = TRUE))
+
+
+# get location of peak
+s2d <- with(cli_sizes, kde2d(Size_1, Size_2, n = 100))
+s2d_peak <- which(s2d$z == max(s2d$z))
+Size_1_peak_at <- round(s2d$x[s2d_peak %% 100], 1)
+xend <- Size_1_peak_at + 100
+Size_2_peak_at <- round(s2d$y[s2d_peak %/% 100], 1)
+yend <- Size_2_peak_at + 100
 
 ggplot(cli_sizes, aes(Size_1, Size_2)) +
   geom_point(alpha = 0.5, size = 2) +
   stat_density2d() +
+  annotate("segment", x = Size_1_peak_at, y = Size_2_peak_at,
+           xend = xend, yend = yend, color = "red", size = 1) +
+  annotate("text", xend, yend, label = paste0("(", Size_1_peak_at, ", ", Size_2_peak_at, ")"), vjust = 0) +
   ggtitle(paste0("Size of Cluster 2 by Size of Cluster 1 for k=", num_clusters, ", # tries=", num_trys))
 
 ## reading off of plot, pick seeds near one of three maxima with cluster rank = order
@@ -46,7 +63,15 @@ ggplot(cli_sizes, aes(Size_1, Size_2)) +
 #  2nd: @(940, 385): 215
 #  3rd: @(1250, 90): 129
 
+# find closest row in cli_trys (with rank == num)
 
+cli_best <- cli_sizes %>%
+  filter(c1 == 1 & c2 == 2) %>%    ## just look at solutions with clusters in decending sizes
+  mutate(distance = sqrt((Size_1 - Size_1_peak_at)^2 + (Size_2 - Size_2_peak_at)^2)) %>%
+  dplyr::select(-starts_with("c")) %>%
+  arrange(distance)
+
+cli_best
 
 
 
